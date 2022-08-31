bd73fd46c0117676698c4393f08e881b7273032f
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2022-08-31 10:50:34 -0300

    Revert "Filter funcionando"
    This reverts commit 04cf045f0e0a506da369de2825d8b9692f359c82.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,27 +2,15 @@ import { Cart, Clock, CoffeeIcon, CoffeeList, Filter, HomeContainer, Icon, Image
 import imageCoffee from "../../assets/imageCoffee.svg"
 import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
 import { CoffeeCardContainer } from "./components/CoffeeCard";
-import { useContext, useEffect, useState } from "react";
-import { CoffeeCard, PurchaseContext } from "../../contexts/PurchaseContext";
-
-export function Home() {
-
-    const { coffeeList } = useContext(PurchaseContext)
-    const [filtered, setFiltered] = useState<CoffeeCard[]>([])
-    const [activeFilter, setActiveFilter] = useState('')
+import { useContext } from "react";
+import { PurchaseContext } from "../../contexts/PurchaseContext";
 
 
-    useEffect(() => {
-        if (activeFilter === '') {
-            setFiltered(coffeeList)
-            return
-        }
-        const filtered = coffeeList.filter((coffee) =>
-            coffee.category.includes(activeFilter))
-        setFiltered(filtered)
-    }, [activeFilter])
+export function Home(){
 
-    return (
+    const {coffeeList} = useContext(PurchaseContext)
+
+    return(
         <HomeContainer>
             <Intro>
                 <Infos>
@@ -31,39 +19,38 @@ export function Home() {
                         <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
                     </Title>
                     <Items>
-                        <p><Cart><ShoppingCart weight="fill" /></Cart>Compra simples e segura</p>
-                        <p><Pack><Package weight="fill" /></Pack>Embalagem mantém o café intacto</p>
-                        <p><Clock><Timer weight="fill" /></Clock>Entrega rápida e rastreada</p>
-                        <p><CoffeeIcon><Coffee weight="fill" /></CoffeeIcon>O café chega fresquinho até você</p>
+                        <p><Cart><ShoppingCart weight="fill"/></Cart>Compra simples e segura</p>
+                        <p><Pack><Package weight="fill"/></Pack>Embalagem mantém o café intacto</p>
+                        <p><Clock><Timer weight="fill"/></Clock>Entrega rápida e rastreada</p>
+                        <p><CoffeeIcon><Coffee weight="fill"/></CoffeeIcon>O café chega fresquinho até você</p>
                     </Items>
                 </Infos>
                 <Image>
-                    <img src={imageCoffee} />
+                    <img src={imageCoffee}/>
                 </Image>
             </Intro>
 
             <CoffeeList>
                 <header>
-                    <h1>Nossos Cafés</h1>
-                    <Filter>
-                        <button onClick={() => setActiveFilter('')}>TODOS</button>
-                        <button onClick={() => setActiveFilter('TRADICIONAL')}>TRADICIONAL</button>
-                        <button onClick={() => setActiveFilter('ESPECIAL')}>ESPECIAL</button>
-                        <button onClick={() => setActiveFilter('COM LEITE')}>COM LEITE</button>
-                        <button onClick={() => setActiveFilter('ALCOÓLICO')}>ALCOÓLICO</button>
-                        <button onClick={() => setActiveFilter('GELADO')}>GELADO</button>
-                    </Filter>
+                <h1>Nossos Cafés</h1>
+                <Filter>
+                    <a href="#">TRADICIONAL</a>
+                    <a href="#">ESPECIAL</a>
+                    <a href="#">COM LEITE</a>
+                    <a href="#">ALCOÓLICO</a>
+                    <a href="#">GELADO</a>
+                </Filter>
                 </header>
                 <List>
-                    {filtered.map(coffee => {
+                    {coffeeList.map(coffee => {
                         return (
-                            <CoffeeCardContainer key={coffee.id}
-                                id={coffee.id}
-                                name={coffee.name}
-                                description={coffee.description}
-                                category={coffee.category}
-                                value={coffee.value}
-                                img={coffee.img} />
+                            <CoffeeCardContainer key={coffee.id} 
+                            id={coffee.id}
+                            name={coffee.name} 
+                            description={coffee.description} 
+                            category={coffee.category}
+                            value={coffee.value}
+                            img={coffee.img}/>
                         )
                     })}
                 </List>
